bf3e4cd452447b80825cb18d4a63d99b9c1a1042
anouar <anouar.ab95@gmail.com>
2025-01-06 16:52:52 +0100

    first commit

---
+++ Orphelins/src/main/java/com/gestion/Orphelins/OrphelinsApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.Orphelins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrphelinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrphelinsApplication.class, args);
+	}
+
+}
