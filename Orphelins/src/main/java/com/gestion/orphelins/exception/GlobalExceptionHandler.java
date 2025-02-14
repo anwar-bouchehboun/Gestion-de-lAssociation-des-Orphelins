9084e4fc807541037fd241922a5583f0d2b318f7
anouar <anouar.ab95@gmail.com>
2025-02-14 17:01:29 +0100

    genner function 403

--- Orphelins/src/main/java/com/gestion/orphelins/exception/GlobalExceptionHandler.java
+++ Orphelins/src/main/java/com/gestion/orphelins/exception/GlobalExceptionHandler.java
@@ -15,91 +15,88 @@ import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.security.access.AccessDeniedException;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        log.error("Erreur de validation des arguments: {}", ex.getMessage());
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        errors.put("message", "Erreur de validation des arguments");
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+                log.error("Erreur de validation des arguments: {}", ex.getMessage());
+                Map<String, String> errors = new HashMap<>();
+                ex.getBindingResult().getFieldErrors()
+                                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+                errors.put("message", "Erreur de validation des arguments");
+                return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        }
 
-    @ExceptionHandler(ValidationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException ex) {
-        ReponseMessage.errors(
-                "Erreur de validation des arguments: " + ex.getMessage());
-        Map<String, String> errors = new HashMap<>();
-        errors.put("error", ex.getMessage());
-        errors.put("message", ex.getMessage());
-        return ResponseEntity.badRequest().body(errors);
-    }
+        @ExceptionHandler(ValidationException.class)
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public ResponseEntity<Map<String, String>> handleValidationException(ValidationException ex) {
+                ReponseMessage.errors(
+                                "Erreur de validation des arguments: " + ex.getMessage());
+                Map<String, String> errors = new HashMap<>();
+                errors.put("error", ex.getMessage());
+                errors.put("message", ex.getMessage());
+                return ResponseEntity.badRequest().body(errors);
+        }
 
-    @ExceptionHandler(NoyFoundExceptionElement.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Map<String, String>> handleNotFoundException(NoyFoundExceptionElement ex, String element) {
-        log.error("Element  non trouvé: {}", ex.getMessage());
-        ReponseMessage.errors(
-                "Element non trouvé: " + element);
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", "Element non trouvé: " + element);
-        errorResponse.put("message", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
+        @ExceptionHandler(NoyFoundExceptionElement.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        public ResponseEntity<Map<String, String>> handleNotFoundException(NoyFoundExceptionElement ex,
+                        String element) {
+                log.error("Element  non trouvé: {}", ex.getMessage());
+                ReponseMessage.errors(
+                                "Element non trouvé: " + element);
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("error", "Element non trouvé: " + element);
+                errorResponse.put("message", ex.getMessage());
+                return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
 
-    @ExceptionHandler(NotFoundExceptionHndler.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundExceptionHndler ex) {
-        log.error("Element  non trouvé: {}", ex.getMessage());
-        ReponseMessage.errors(
-                "Element non trouvé: " + ex.getMessage());
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", "Element non trouvé: ");
-        errorResponse.put("message", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
+        @ExceptionHandler(NotFoundExceptionHndler.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundExceptionHndler ex) {
+                log.error("Element  non trouvé: {}", ex.getMessage());
+                ReponseMessage.errors(
+                                "Element non trouvé: " + ex.getMessage());
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("error", "Element non trouvé: ");
+                errorResponse.put("message", ex.getMessage());
+                return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, Object>> handleConstraintViolation(
-            ConstraintViolationException ex) {
+        @ExceptionHandler(ConstraintViolationException.class)
+        public ResponseEntity<Map<String, Object>> handleConstraintViolation(
+                        ConstraintViolationException ex) {
 
-        Map<String, Object> errors = new HashMap<>();
-        errors.put("status", HttpStatus.BAD_REQUEST.value());
-        errors.put("message", "Erreur de validation");
+                Map<String, Object> errors = new HashMap<>();
+                errors.put("status", HttpStatus.BAD_REQUEST.value());
+                errors.put("message", "Erreur de validation");
 
-        Map<String, String> validationErrors = ex.getConstraintViolations()
-                .stream()
-                .collect(Collectors.toMap(
-                        violation -> violation.getPropertyPath().toString(),
-                        violation -> violation.getMessage(),
-                        (error1, error2) -> error1));
+                Map<String, String> validationErrors = ex.getConstraintViolations()
+                                .stream()
+                                .collect(Collectors.toMap(
+                                                violation -> violation.getPropertyPath().toString(),
+                                                violation -> violation.getMessage(),
+                                                (error1, error2) -> error1));
 
-        errors.put("errors", validationErrors);
+                errors.put("errors", validationErrors);
 
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(errors);
-    }
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(errors);
+        }
 
-    /*
-     * @ExceptionHandler(BadCredentialsException.class)
-     * 
-     * @ResponseStatus(HttpStatus.UNAUTHORIZED)
-     * public ResponseEntity<ErrorResponse>
-     * handleBadCredentialsException(BadCredentialsException ex) {
-     * ErrorResponse errorResponse = new ErrorResponse(LocalDate.now(),
-     * "The identifications are wrong",
-     * HttpStatus.BAD_REQUEST.value(), null);
-     * return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-     * }
-     * 
-     */
+        @ExceptionHandler(AccessDeniedException.class)
+        @ResponseStatus(HttpStatus.FORBIDDEN)
+        public ResponseEntity<Map<String, String>> handleAccessDeniedException(AccessDeniedException ex) {
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("status", "error");
+                errorResponse.put("message", "Accès refusé : vous n'avez pas les autorisations nécessaires");
+                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+        }
 
 }
